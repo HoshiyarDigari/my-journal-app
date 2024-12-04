9ceae2ecdffe92c4b6c612a2ddc7eb2e77596fb9
HoshiyarDigari <hoshiyarsingh@gmail.com>
2024-12-04 11:46:20 -0500

    added signup button and functions to handle login vs sign up from the login page

--- users.py
+++ users.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from models import User
 
 user_bp=Blueprint('user',__name__)
 
-@user_bp.route('/login', methods=['GET'])
+@user_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        # if the request is for login
+        if request.values.get('login'):
+            return redirect('/journal')
+        elif request.values.get('sign-up'):
+            return "you are registered"
+    
