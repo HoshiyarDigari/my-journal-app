c49d466f76d00f44526ea88c815da43c07ddcf43
HoshiyarDigari <hoshiyarsingh@gmail.com>
2024-12-09 13:15:53 -0500

    added function to update journalDate to current date after /journal loads

--- static/js/journal.js
+++ static/js/journal.js
@@ -1,8 +1,15 @@
 //  function to populate date on journal landing page to the current date
 
-addEventListener(document.onload, setDate())
+addEventListener('DOMContentLoaded', setDate)
 function setDate(){
-    console.log(Date.now);
+    // Date object converts  Date.now() which is epoch time to 'Mon Dec 09 2024 12:57:33 GMT-0500 (Eastern Standard Time)
+    const now = new Date(Date.now());
+    // converting this to the format accepted by the Date HTML element
+    formattedDate = now.toISOString().split('T')[0];
+    formattedTime = now.toISOString().split('T')[1];
+
+    document.getElementById('journalDate').value = formattedDate;
+    
     
 
 }
